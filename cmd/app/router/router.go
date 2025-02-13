368e32298f0a975b09831ff1a36316d0e4649305
spashev.nurken <s.nurken92@gmail.com>
2025-02-13 13:15:03 +0400

    pagiante products list

--- cmd/app/router/router.go
+++ cmd/app/router/router.go
@@ -20,11 +20,11 @@ func RegisterRoutes(db *sqlx.DB, tokenMaker *token.JWTMaker) *fiber.App {
 
 	apiV1 := app.Group("/api/v1")
 	{
-		apiV1.Post("/register", middleware.Paginate, handler.Register(db, tokenMaker))
+		apiV1.Post("/register", handler.Register(db, tokenMaker))
 		apiV1.Post("/login", handler.Login(db, tokenMaker))
 		apiV1.Get("/users/me", handler.UserMe(db, tokenMaker))
 
-		apiV1.Get("/products/get", handler.GetProductList(db))
+		apiV1.Get("/products/get", middleware.Paginate, handler.GetProductList(db))
 		apiV1.Get("/products/:slug", handler.GetProductBySlug(db))
 		apiV1.Get("/categories", handler.GetCategories(db))
 	}
