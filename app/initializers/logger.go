cd4a7bf78e982fbd8614b8ad2a4814542f14c4b1
spashev.nurken <s.nurken92@gmail.com>
2025-02-26 11:09:28 +0400

    logger info

--- app/initializers/logger.go
+++ app/initializers/logger.go
@@ -1,12 +1,22 @@
 package initializers
 
 import (
+	"os"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func NewLogger() fiber.Handler {
 	return logger.New(logger.Config{
-		Format: "[${pid} ${locals:requestid}] ${ip}:${port} ${status} - ${method} ${path}\n",
+		Next:          nil,
+		Done:          nil,
+		Format:        "${time} | ${pid} | ${ip}:${port} | ${status}| ${method} | ${path} |${latency} | ${error}\n",
+		TimeFormat:    "15:04:05",
+		TimeZone:      "Local",
+		TimeInterval:  500 * time.Millisecond,
+		Output:        os.Stdout,
+		DisableColors: false,
 	})
 }
